8ac6911650b2ccbbaba4a735aaa07c8c9d15b8a5
tohanici <tohanici@gmail.com>
2024-10-01 12:03:48 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        double price = 35.80;
+        double tax = 2.30;
+        int quantity = 10;
+        double total = 0;
+        String message;
+        message = "i want to buy " + quantity + " shirt!";
+        total = price + tax + quantity;
+        System.out.println(message);
+        System.out.println("Total cost with tax is: " + total );
+
+    }
+}
